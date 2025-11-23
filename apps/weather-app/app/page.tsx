b3f2dae5de6d22078022d209100a40ec247bbf84
Jason Bell <jasonbell143@gmail.com>
2025-11-23 20:59:48 +0000

    Revert "commits RefetchButton for visibility in process"
    This reverts commit 8e0e9a980325385caa3ccf6ffc021079dd3dd8ad.

--- apps/weather-app/app/page.tsx
+++ apps/weather-app/app/page.tsx
@@ -1,14 +1,13 @@
+import styles from "./page.module.css";
 import { fetchWeather } from "./api/weather/fetchWeather";
-import RefetchButton from "./components/RefetchButtonTEMP";
-import { DataProvider } from "./providers/WeatherDataContext/DataContext";
 
 export default async function Index() {
   // Initial server-side call to have first-paint with SSR data
   const weatherData = await fetchWeather("Brighton");
 
   return (
-    <DataProvider initialData={weatherData}>
-      <RefetchButton />
-    </DataProvider>
+    <div className={styles.page}>
+      <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+    </div>
   );
 }
