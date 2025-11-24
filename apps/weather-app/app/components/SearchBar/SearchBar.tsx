fb05ed8a8a5b1e98cef0a392310f360591c830f6
Jason Bell <jasonbell143@gmail.com>
2025-11-24 20:52:56 +0000

    Adds very basic loading spinner, to indicate when a search is happening

--- apps/weather-app/app/components/SearchBar/SearchBar.tsx
+++ apps/weather-app/app/components/SearchBar/SearchBar.tsx
@@ -6,13 +6,32 @@ import { useLocationChange } from "@/app/providers/WeatherDataContext/useLocatio
 export default function SearchBar() {
   const [searchValue, setSearchValue] = useState("");
   const onLocationChange = useLocationChange();
+
+  /** Basic loading indicator - ideally this is contained within the context/data,
+   * but without a full loading state then has less use, and my goal is mostly to
+   * show a user that the search has gone through
+   */
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <form
       className={styles.searchBar}
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         // prevent navigation
         e.preventDefault();
-        onLocationChange(searchValue);
+
+        // Don't search if no value is present
+        if (!searchValue) return;
+
+        setSearchValue("");
+        setIsLoading(true);
+        try {
+          await onLocationChange(searchValue);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setIsLoading(false);
+        }
       }}
     >
       <input
@@ -22,8 +41,16 @@ export default function SearchBar() {
           setSearchValue(e.target.value);
         }}
       />
-      <button type="submit" aria-label="Search for weather data">
-        <img src={"/arrow.svg"} />
+      <button
+        type="submit"
+        aria-label="Search for weather data"
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <div className={styles.loadingSpinner} />
+        ) : (
+          <img src={"/arrow.svg"} />
+        )}
       </button>
     </form>
   );
