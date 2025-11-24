2092645312f380e5ade09550474563bf55f8e393
Jason Bell <jasonbell143@gmail.com>
2025-11-24 18:26:30 +0000

    Adds functionality to TemperatureUnitSelector by controlling radio inputs

--- apps/weather-app/app/components/TemperatureUnitSelector/TemperatureUnitSelector.tsx
+++ apps/weather-app/app/components/TemperatureUnitSelector/TemperatureUnitSelector.tsx
@@ -1,28 +1,40 @@
 "use client";
+import { useTemperatureUnitChange } from "@/app/providers/TemperatureUnitProvider/useTemperatureUnitChange";
 import styles from "./TemperatureUnitSelector.module.scss";
+import { useTemperatureFormat } from "@/app/providers/TemperatureUnitProvider/useTemperatureFormat";
+import { TemperatureUnit } from "@/app/utils/celsiusToUnitStr";
 
+/** A Component that returns 2 radio buttons, allowing the user to set
+ * the temperatureUnit in the TemperatureContext
+ */
 export default function TemperatureUnitSelector() {
+  const onTemperatureUnitChange = useTemperatureUnitChange();
+  const { temperatureUnit } = useTemperatureFormat();
+
+  const radioDetails: { value: TemperatureUnit; label: string }[] = [
+    { value: "celsius", label: "°C" },
+    { value: "fahrenheit", label: "°F" },
+  ];
+
   return (
     <div className={styles.temperatureSelectors}>
-      <label>
-        <input
-          type="radio"
-          value={"Celsius"}
-          className={"visually-hidden"}
-          name="temperature-unit-radios"
-          defaultChecked
-        />
-        C
-      </label>
-      <label>
-        <input
-          type="radio"
-          value={"Fahrenheit"}
-          className={"visually-hidden"}
-          name="temperature-unit-radios"
-        />
-        F
-      </label>
+      {radioDetails.map(({ value, label }) => {
+        return (
+          <label key={value}>
+            <input
+              type="radio"
+              value={"celsius"}
+              className={"visually-hidden"}
+              name="temperature-unit-radios"
+              checked={temperatureUnit === value}
+              onChange={() => {
+                onTemperatureUnitChange(value);
+              }}
+            />
+            {label}
+          </label>
+        );
+      })}
     </div>
   );
 }
